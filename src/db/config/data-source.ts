7703a83a54b09ff3a757922d299bc387a24fe5e8
Celso Oliva Aparecido <celso.oliva.aparecido@gmail.com>
2022-06-11 02:51:15 +0000

    fix(db/data-source): rewrites to use environment vars in config

--- src/db/config/data-source.ts
+++ src/db/config/data-source.ts
@@ -3,15 +3,15 @@ import { DataSource } from 'typeorm';
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: 'postgres',
-  port: 5432,
-  username: 'cdb_dev',
-  password: 'cdb_Dev@User',
-  database: 'cdbrag-db',
+  host: process.env.POSTGRES_HOSTNAME || 'postgres',
+  port: parseInt(process.env.POSTGRES_PORT || '5432', 10),
+  username: process.env.POSTGRES_USER || 'user',
+  password: process.env.POSTGRES_PASSWORD || 'password',
+  database: process.env.POSTGRES_DB || 'db',
   synchronize: false,
   cache: true,
   logging: false,
   entities: ['src/db/entity/**/*.ts'],
   migrations: ['src/db/migration/**/*.ts'],
-  subscribers: ['src/subscriber/**/*.ts'],
+  subscribers: ['src/db/subscriber/**/*.ts'],
 });
