305f4057d70d72164e502631fab8cd411acc7b77
Celso Oliva Aparecido <celso.oliva.aparecido@gmail.com>
2022-06-12 18:47:24 -0300

    chore: adds timeout function to mock database

--- src/tests/test-config/run-test-db.ts
+++ src/tests/test-config/run-test-db.ts
@@ -1,11 +1,11 @@
 import pgSetup from '@databases/pg-test/jest/globalSetup';
 import pgTeardown from '@databases/pg-test/jest/globalTeardown';
 
-export async function runTestDatabase() {
+export async function runTestDatabase(timeout?: number) {
   await pgSetup({
     externalPort: parseInt(process.env.PG_TEST_PORT || '5433', 10),
     debug: false,
-    connectTimeoutSeconds: 60,
+    connectTimeoutSeconds: timeout || 20,
     detached: true,
     environment: {
       POSTGRES_PORT: process.env.PG_TEST_PORT || '5433',
