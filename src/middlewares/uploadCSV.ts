522d14a2f419169cf4e0d2d692efb6b3c5c9c9ae
Celso Oliva Aparecido <celso.oliva.aparecido@gmail.com>
2022-06-12 14:40:28 -0300

    refact(uploadCSV): adds 5MB limit to size to prevents abuse

--- src/middlewares/uploadCSV.ts
+++ src/middlewares/uploadCSV.ts
@@ -1,4 +1,9 @@
 import os from 'os';
 import multer from 'multer';
 
-export const uploadCSV = multer({ dest: os.tmpdir() });
+export const uploadCSV = multer({
+  dest: os.tmpdir(),
+  limits: {
+    fileSize: 5 * 1024 * 1024, // no larger than 5 MB
+  },
+});
